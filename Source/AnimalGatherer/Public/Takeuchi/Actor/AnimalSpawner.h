2c95a94740ad7a5520a017a4bd10cffa581119ba
AyaTakeuchi <jagapo1129@gmail.com>
2026-07-03 10:24:08 +0900

    feature:キャラクターを生成するスポナーの作成

--- Source/AnimalGatherer/Public/Takeuchi/Actor/AnimalSpawner.h
+++ Source/AnimalGatherer/Public/Takeuchi/Actor/AnimalSpawner.h
@@ -1,26 +1,69 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
 #include "AnimalSpawner.generated.h"
 
 UCLASS()
 class ANIMALGATHERER_API AAnimalSpawner : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AAnimalSpawner();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+	//生成する動物のクラス
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner")
+	TSubclassOf<APawn> AnimalClass;
 
+	//動物を出す座標
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner")
+	FVector SpawnLocation = FVector::ZeroVector;
+
+	//動物を出す向き
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner")
+	FRotator SpawnRotation = FRotator::ZeroRotator;
+
+	//何秒ごとに生成するか
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner", meta = (ClampMin = "0.1", UIMin = "0.1"))
+	float SpawnInterval = 3.0f;
+
+	//ステージに存在できる動物の数制限
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner", meta = (ClampMin = "1", UIMin = "1"))
+	int32 MaxAnimalCount = 5;
+
+	//ゲーム開始時にスポーンを始めるかどうか
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animal Spawner")
+	bool bStartSpawningOnBeginPlay = true;
+
+	//スポーン処理を開始する関数
+	UFUNCTION(BlueprintCallable, Category = "Animal Spawner")
+	void StartSpawning();
+
+	//スポーン処理を停止する関数
+	UFUNCTION(BlueprintCallable, Category = "Animal Spawner")
+	void StopSpawning();
+
+	//動物を生成する関数
+	UFUNCTION(BlueprintCallable, Category = "Animal Spawner")
+	APawn* SpawnAnimal();
+
+	//現在生きている動物の数を返す
+	UFUNCTION(BlueprintPure, Category = "Animal Spawner")
+	int32 GetSpawnedAnimalCount() const;
+
+private:
+	void TrySpawnAnimal();
+	void CleanupSpawnedAnimals();
+
+	UPROPERTY()
+	TArray<TObjectPtr<APawn>> SpawnedAnimals;
+
+	FTimerHandle SpawnTimerHandle;
 };
